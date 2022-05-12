d8201f7db3ddf1268b58db06ca3c8fd466e09aff
trenfranco <trenfranco@gmail.com>
2022-05-12 16:32:10 -0700

    swp func coreection

--- swap.c
+++ swap.c
@@ -13,11 +13,8 @@ void _swap(stack_t **stack, unsigned int line_number)
 	stack_t *a, *b, *c;
 
 	a = *stack;
-	if (a->next == NULL || !stack || !(*stack))
+	if (*stack != NULL && (*stack)->next != NULL)
 	{
-		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short", line_number);
-		exit(EXIT_FAILURE);
-	}
 	b = a->next;
 	c = a->next->next;
 
@@ -28,5 +25,11 @@ void _swap(stack_t **stack, unsigned int line_number)
 	a->next = b->next;
 	b->next = *stack;
 	*stack = b;
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 }
